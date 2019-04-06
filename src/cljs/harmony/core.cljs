3e3525c722a590ad3bf0a0d1aca7b0cbc5fbc63a
mrmikardo <jackwestmore@gmail.com>
2019-04-06 21:34:54 +0300

    Handle effects for other patterns

--- src/cljs/harmony/core.cljs
+++ src/cljs/harmony/core.cljs
@@ -31,20 +31,26 @@
 
 (rf/reg-event-db
  :server/init
- (fn [_ [_ initial-db]]
-   initial-db))
+ (fn [db [_ initial-db]]
+   (assoc db :server initial-db)))
 
 (rf/reg-event-db
  :server/button-on
  (fn [db [_ voice beat-index]]
-   (assoc-in db [:sequences voice beat-index] 1)))
+   (assoc-in db [:server :sequences voice beat-index] 1)))
 
 (rf/reg-event-db
  :server/button-off
  (fn [db [_ voice beat-index]]
-   (assoc-in db [:sequences voice beat-index] 0)))
+   (assoc-in db [:server :sequences voice beat-index] 0)))
 
 ;; UI EVENTS
+
+(rf/reg-event-db
+  :init
+  (fn [db _]
+    (-> db
+        (assoc :selected-voice :kick))))
 
 (rf/reg-event-fx
  :button-on
@@ -56,12 +62,23 @@
  (fn [{:keys [db]} [_ voice beat-index]]
    {:send [:button-off voice beat-index]}))
 
+(rf/reg-event-db
+  :select-voice
+  (fn [db [_ voice]]
+    (assoc db :selected-voice voice)))
+
+
 ;; SUBSCRIPTIONS
 
 (rf/reg-sub
  ::sequence
  (fn [db [_ voice]]
-   (-> db :sequences voice)))
+   (-> db :server :sequences voice)))
+
+(rf/reg-sub
+  ::selected-voice
+  (fn [db _]
+    (:selected-voice db)))
 
 ;; COMPONENTS
 
@@ -73,48 +90,55 @@
             (when on? "keybutton-pressed")]}
    [:div.button-light]])
 
-(defn pattern-button [{:keys [instrument selected?]}]
+(defn pattern-button [{:keys [voice selected?]} & children]
   [:div
-   {:class ["instrument-button"
+   {:on-click #(rf/dispatch [:select-voice voice])
+    :class ["instrument-button"
             (when selected? "instrument-button-pressed")]}
-   instrument])
+   children]) 
 
 (defn tempo [bpm]
   [:div.tempocontainer [:div.tempo-display bpm]])
 
 (defn app []
-  [:div.main
-   [:h1 "h a r m o n y"]
-   [tempo 50] ; dummy value
-   [:div.slider-wrapper [:input {:type "range"}]
-                     :min 1
-                     :max 100
-                     :value 50
-                     :id "frequency"]
-   [:div.slider-wrapper [:input {:type "range"}]
-                     :min 1
-                     :max 100
-                     :value 50
-                     :id "amplitude"]
-   [:div.patternkeyscontainer
-    [pattern-button {:instrument "KICK" :selected? true}]
-    [pattern-button {:instrument "SNARE" :selected? true}]
-    [pattern-button {:instrument "CLOSED-HAT" :selected? true}]
-    [pattern-button {:instrument "OPEN-HAT" :selected? true}]
-    [pattern-button {:instrument "TOM" :selected? true}]
-    [pattern-button {:instrument "CLAP" :selected? true}]]
-   [:div.keyscontainer
-    (for [[kick-val beat] (map list @(rf/subscribe [::sequence :kick]) (range 0 16))]
-      [sequencer-button 
-       {:key beat
-        :bar (inc (Math/floor (/ beat bars)))
-        :on? (= kick-val 1)
-        :on-click #(rf/dispatch [(if (= kick-val 1) ::button-off ::button-on) :kick beat])}])]])
+  (let [selected @(rf/subscribe [::selected-voice])
+        selected-seq @(rf/subscribe[::sequence selected])]
+    [:div.main
+     [:h1 "h a r m o n y"]
+     [tempo 50] ; dummy value
+     [:div.slider-wrapper [:input {:type "range"}]
+                       :min 1
+                       :max 100
+                       :value 50
+                       :id "frequency"]
+     [:div.slider-wrapper [:input {:type "range"}]
+                       :min 1
+                       :max 100
+                       :value 50
+                       :id "amplitude"]
+     [:div.patternkeyscontainer
+        (for [voice [:kick :snare :closed-hat :open-hat :tom :clap]]
+          [pattern-button {:key voice
+                           :voice voice
+                           :selected? (= voice selected)}
+             (name voice)])]
+     [:div.keyscontainer
+      (for [[kick-val beat] (map list selected-seq (range 0 16))]
+        [sequencer-button 
+         {:key beat
+          :bar (inc (Math/floor (/ beat bars)))
+          :on? (= kick-val 1)
+          :on-click #(rf/dispatch [(if (= kick-val 1) 
+                                     :button-off 
+                                     :button-on) 
+                                   selected 
+                                   beat])}])]]))
 
 (defn mount-root []
   (r/render [app] (.getElementById js/document "app")))
 
 (defn init! []
+  (rf/dispatch-sync [:init])
   (mount-root))
 
 
