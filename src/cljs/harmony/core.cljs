9982b2a3181ad2f1173e3a2f6a57e7ca95e7068e
mrmikardo <jackwestmore@gmail.com>
2019-04-06 19:00:18 +0300

    Changes to UI

--- src/cljs/harmony/core.cljs
+++ src/cljs/harmony/core.cljs
@@ -3,8 +3,53 @@
    [reagent.core :as r]
    [re-frame.core :as rf]))
 
+(defn button [{:keys [bar on?]}]
+  [:div
+   {:class ["keybutton"
+            (if (even? bar) "keybutton-pink" "keybutton-purple")
+            (when on? "keybutton-pressed")]}
+   [:div.button-light]])
+
+(defn tempo [bpm]
+  [:div.tempo-display bpm])
+
 (defn app []
-  [:h1 "Harmony"])
+  (fn []
+    [:span.main
+     [:h1 "h a r m o n y"]
+     (comment[:slidercontainer [:input {:type "range"} 
+                                :min 1 
+                                :max 100
+                                :value 50
+                                :class "slider"
+                                :id "frequency"]])
+     (comment "you need another div here")
+     (comment[:slidercontainer [:input {:type "range"} 
+                                :min 1 
+                                :max 100
+                                :value 50
+                                :class "slider"
+                                :id "amplitude"]])
+     [:div.tempocontainer [tempo 50]] ; dummy value
+     [:div.keyscontainer
+      [button {:bar 1 :on? false}] 
+      [button {:bar 1 :on? false}] 
+      [button {:bar 1 :on? false}] 
+      [button {:bar 1 :on? false}] 
+      [button {:bar 2 :on? false}] 
+      [button {:bar 2 :on? false}] 
+      [button {:bar 2 :on? false}] 
+      [button {:bar 2 :on? false}] 
+      [button {:bar 3 :on? true}] 
+      [button {:bar 3 :on? false}] 
+      [button {:bar 3 :on? false}] 
+      [button {:bar 3 :on? false}] 
+      [button {:bar 4 :on? false}] 
+      [button {:bar 4 :on? false}] 
+      [button {:bar 4 :on? false}] 
+      [button {:bar 4 :on? false}]]])) 
+     
+
 
 (defn mount-root []
   (r/render [app] (.getElementById js/document "app")))
