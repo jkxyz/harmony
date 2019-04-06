fa92623cbbef93a868e8691763b69b5d8fd36882
Josh Kingsley <josh@joshkingsley.me>
2019-04-06 21:54:40 +0300

    schedule all voices

--- src/clj/harmony/overtone.clj
+++ src/clj/harmony/overtone.clj
@@ -1,7 +1,7 @@
 (ns harmony.overtone
   (:require
    [overtone.live :as overtone :refer [metronome at apply-by]]
-   [overtone.inst.drum :refer [kick]]))
+   [overtone.inst.drum :refer [kick closed-hat open-hat tom clap snare]]))
 
 (def sequence-length 16)
 
@@ -25,16 +25,21 @@
 (defn beat-offset [index]
   (+ (Math/floor (/ index 4)) (/ (mod index 4) 4)))
 
-(defn schedule-kick [first-beat nome]
+(defn schedule-voice [voice first-beat nome f]
   (dotimes [index sequence-length]
-    (when-not (= 0 (get (-> @db :sequences :kick) index))
-      (at (nome (+ first-beat (beat-offset index))) (kick)))))
+    (when (= 1 (get-in @db [:sequences voice index]))
+      (at (nome (+ first-beat (beat-offset index))) (f)))))
 
 (defn sequence-loop [nome]
   (let [first-beat (nome)
         next-first-beat (+ first-beat 4)]
     (println "Loop start:" first-beat)
-    (schedule-kick first-beat nome)
+    (schedule-voice :kick first-beat nome #(kick))
+    (schedule-voice :open-hat first-beat nome #(open-hat))
+    (schedule-voice :closed-hat first-beat nome #(closed-hat))
+    (schedule-voice :tom first-beat nome #(tom))
+    (schedule-voice :clap first-beat nome #(clap))
+    (schedule-voice :snare first-beat nome #(snare))
     (apply-by (nome next-first-beat) #'sequence-loop [nome])))
 
 (comment
